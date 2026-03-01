59075ae919bbf18c1e4a9eb3e7132de2c1e944c2
deeksha <deeksha@thelevel.ai>
2026-03-01 14:48:24 +0530

    added test case using loops

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,4 +1,5 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+const { text } = require('node:stream/consumers');
 
 
 test('Browser Context Playwright test',async ({browser})=>{
@@ -30,7 +31,7 @@ test('Page Playwright test',async ({page})=>{
     console.log(await page.locator('.card-body b').allTextContents()); 
 });
 
-test.only("Child window handling", async({page,context})=>{
+test("Child window handling", async({page,context})=>{
     await page.goto("https://www.google.com/");
     await page.locator("#APjFqb").fill("orange");
     await page.locator("#jZ2SBf").click();
@@ -39,4 +40,54 @@ test.only("Child window handling", async({page,context})=>{
         page.locator("[aria-label='Orange (fruit) - Wikipedia. Opens in new tab.']").click()]);
     console.log(await new_Page.locator("#firstHeading").textContent());
 
+});
+
+test.only("End to end order placement flow ", async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.locator('#userEmail').fill("emp1112.user7@example.com");
+    await page.locator('#userPassword').fill("Password321@");
+    await page.locator('#login').click();
+    await page.locator(".card-body b").first().waitFor();
+   const count = await page.locator(".card").count();
+   for(let i = 0; i < count; i++){
+        if(await page.locator(".card").nth(i).locator("b").textContent()==="ZARA COAT 3"){
+            await page.locator(".card").nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+   }
+   await page.locator("[routerlink*='cart']").click();
+   await page.locator(".infoWrap").waitFor();
+   expect(await page.locator("text=ZARA COAT 3").isVisible()).toBeTruthy();
+   await page.locator("text=Checkout").click();
+   await page.getByPlaceholder("Select Country").pressSequentially("ind",{delay:150});
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " India") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
+ 
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+ 
+ 
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
+ 
 });
